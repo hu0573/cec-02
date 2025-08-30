2de4455fc9bf168b6d3c5e1f85962fa1648ae171
Ruiqing Hu <huruiqing0011@gmail.com>
2025-08-30 21:22:11 +0930

    set badge

--- popup.js
+++ popup.js
@@ -1,3 +1,12 @@
 const timeElement = document.getElementById("time");
 const currentTime = new Date().toLocaleTimeString();
 timeElement.textContent = `Time is: ${currentTime}`;
+
+chrome.action.setBadgeText(
+  {
+    text: "Time",
+  },
+  () => {
+    console.log("Finished setting badge text.");
+  }
+);

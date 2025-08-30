3cf8552d1a33b287b58a108f66346f13c27ebbdd
Ruiqing Hu <huruiqing0011@gmail.com>
2025-08-31 09:21:07 +0930

    use storage

--- options.js
+++ options.js
@@ -1,5 +1,22 @@
 const nameInput = document.getElementById("input_name");
 const saveInput = document.getElementById("save_input");
 saveInput.addEventListener("click", () => {
-  console.log(nameInput.value);
+  const name = nameInput.value;
+  console.log("before storage");
+  chrome.storage.sync.set(
+    {
+      name: name,
+    },
+    () => {
+      console.log(`name is set to ${name}`);
+      chrome.action.setBadgeText(
+        {
+          text: name,
+        },
+        () => {
+          console.log(`badget is set to ${name}`);
+        }
+      );
+    }
+  );
 });
--- popup.js
+++ popup.js
@@ -1,12 +1,16 @@
 const timeElement = document.getElementById("time");
 const currentTime = new Date().toLocaleTimeString();
+const nameElement = document.getElementById("name");
 timeElement.textContent = `Time is: ${currentTime}`;
 
-chrome.action.setBadgeText(
-  {
-    text: "Time",
-  },
-  () => {
-    console.log("Finished setting badge text.");
+chrome.storage.sync.get(["name"], (res) => {
+  let name; // 先声明在外面
+
+  if (!res.name || res.name.length === 0) {
+    name = "???";
+  } else {
+    name = res.name;
   }
-);
+
+  nameElement.textContent = `Your name is ${name}`;
+});
